9cd2b901a835f90aed8b835ce5e700d61a8e0b56
Ziyoda1304 <ziyodaxon1304@gmail.com>
2026-09-17 11:17:27 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usmon_nosir',  # our app
+    'U.Nosir',  # our app
 ]
 
 MIDDLEWARE = [
